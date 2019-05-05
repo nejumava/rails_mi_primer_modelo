9cd7f6ff4c3f8499af880a3410e169369b028804
Nelly Mahecha <nejumava@gmail.com>
2019-05-05 14:02:34 -0500

    Solucion a reto del primer modelo

---
+++ db/migrate/20190505182740_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.integer :duration
+      t.integer :year
+      t.string :rating
+      t.string :description
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
